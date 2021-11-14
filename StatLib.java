152cfd06fdea4349488d430a80f1acbbf04051b1
idan2021 <idanr2017@gmail.com>
2021-11-14 20:57:04 +0200

    Add var and cov functions

--- StatLib.java
+++ StatLib.java
@@ -3,11 +3,9 @@ package test;
 
 public class StatLib {
 
-	
-
 	// simple average
 	public static float avg(float[] x){
-		int sum = 0;
+		float sum = 0;
 		for(float f : x){
 			sum += f;
 		}
@@ -16,13 +14,25 @@ public class StatLib {
 
 	// returns the variance of X and Y
 	public static float var(float[] x){
-		return 0;
+		float u = avg(x);
+		float sigma = 0;
+		for(float f : x)
+			sigma += f*f;// f^2
+		sigma /= x.length; // *(1/N)
+		// is x.len - 1 ? https://stackoverflow.com/questions/7988486/how-do-you-calculate-the-variance-median-and-standard-deviation-in-c-or-java
+		return sigma - (u*u);
 	}
 
 	// returns the covariance of X and Y
 	public static float cov(float[] x, float[] y){
-
-		return 0;
+		int len = x.length;
+		//if(x.length != y.length) ?!?!?!?!?!
+		float[] covarr = new float[len];
+		float sum = 0;
+		for (int i = 0; i < len; i++){
+			covarr[i] = (x[i] - avg(x))*(y[i] - avg(y));
+		}
+		return avg(covarr);
 	}
 
 
