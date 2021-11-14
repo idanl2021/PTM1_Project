6bc6ff9bf562187f58d0d2881a56b5b6a11991ec
idan2021 <idanr2017@gmail.com>
2021-11-14 20:18:31 +0200

    Add files of first milestone

---
+++ StatLib.java
@@ -0,0 +1,49 @@
+package test;
+
+
+public class StatLib {
+
+	
+
+	// simple average
+	public static float avg(float[] x){
+		int sum = 0;
+		for(float f : x){
+			sum += f;
+		}
+		return sum / x.length;
+	}
+
+	// returns the variance of X and Y
+	public static float var(float[] x){
+		return 0;
+	}
+
+	// returns the covariance of X and Y
+	public static float cov(float[] x, float[] y){
+
+		return 0;
+	}
+
+
+	// returns the Pearson correlation coefficient of X and Y
+	public static float pearson(float[] x, float[] y){
+		return 0;
+	}
+
+	// performs a linear regression and returns the line equation
+	public static Line linear_reg(Point[] points){
+		return null;
+	}
+
+	// returns the deviation between point p and the line equation of the points
+	public static float dev(Point p,Point[] points){
+		return 0;
+	}
+
+	// returns the deviation between point p and the line
+	public static float dev(Point p,Line l){
+		return 0;
+	}
+	
+}
