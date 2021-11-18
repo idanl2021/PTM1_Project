62aa41467c8690f695592ee836123169dbece6a7
idan2021 <idanr2017@gmail.com>
2021-11-18 17:39:12 +0200

    End of milestone 1

--- StatLib.java
+++ StatLib.java
@@ -20,6 +20,7 @@ public class StatLib {
 			sigma += f*f;// f^2
 		sigma /= x.length; // *(1/N)
 		// is x.len - 1 ? https://stackoverflow.com/questions/7988486/how-do-you-calculate-the-variance-median-and-standard-deviation-in-c-or-java
+		//System.out.println(sigma - (u*u));
 		return sigma - (u*u);
 	}
 
@@ -38,22 +39,34 @@ public class StatLib {
 
 	// returns the Pearson correlation coefficient of X and Y
 	public static float pearson(float[] x, float[] y){
-		return 0;
+		float cova = cov(x,y);
+		float sigmaX = (float) Math.sqrt(var(x)); // double?
+		float sigmaY = (float) Math.sqrt(var(y)); // double?
+		return cova/(sigmaX*sigmaY);
 	}
 
 	// performs a linear regression and returns the line equation
 	public static Line linear_reg(Point[] points){
-		return null;
+		float[] x = new float[points.length];
+		float[] y = new float[points.length];
+		for(int i = 0; i < points.length; i++){
+			x[i] = points[i].x;
+			y[i] = points[i].y;
+		}
+		float a = cov(x,y) / var(x);
+		float b = avg(y) - a*avg(x);
+		return new Line(a, b);
 	}
 
 	// returns the deviation between point p and the line equation of the points
 	public static float dev(Point p,Point[] points){
-		return 0;
+		Line line = linear_reg(points);
+		return Math.abs(line.f(p.x) - p.y);
 	}
 
 	// returns the deviation between point p and the line
 	public static float dev(Point p,Line l){
-		return 0;
+		return Math.abs(l.f(p.x) - p.y);
 	}
 	
 }
